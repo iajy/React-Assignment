aeb5651ff1fb5ba7a260c8606df7fc940473e28e
Ajay Binu <ajaybinuajay2002@gmail.com>
2025-05-05 17:11:03 +0530

    inital commit with navigation bar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function NavBar() {
+
+ 
+  return (
+    <>
+    <div className='container hidden fixed bg-blue-500 text-white md:flex justify-between items-center p-4'>
+      <div>
+            <span className='text-2xl font-medium'>Loan Calculator</span>
+      </div>
+      <div className='flex gap-10 justify-center items-center'>
+        <button className='bg-blue-400 px-5 py-1 rounded'>HOME</button>
+        <span>EXCHANGE RATE(LIVE)</span>
+        <span>ABOUT</span>
+        <span>ERROR PAGE</span>
+        <div>
+        dark mode
+        </div>
+      </div>
+      
+      
+    </div>
+    <div className='container flex justify-between items-center md:hidden bg-blue-500 text-white h-30 p-4 '>
+      <div></div>
+      <div>
+            <span className='text-3xl font-medium'>Loan Calculator</span>
+      </div>
+      <div>
+        darkmode
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default NavBar
